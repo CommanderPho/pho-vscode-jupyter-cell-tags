2c710a0f8f007e5bc914235a5b0d4f39e5a64ffe
Pho Hale <PhoHale@gmail.com>
2025-03-09 19:05:34 -0400

    moving towards real methods

--- src/tagProperties/tagPropertiesManager.ts
+++ src/tagProperties/tagPropertiesManager.ts
@@ -1,5 +1,7 @@
 import * as vscode from 'vscode';
 import { TagProperties } from '../models/tagProperties';
+import { updateNotebookMetadata } from '../util/notebookMetadata';
+
 
 export class TagPropertiesManager {
     private static readonly METADATA_KEY = 'tagProperties';
@@ -23,13 +25,33 @@ export class TagPropertiesManager {
         return metadata[this.METADATA_KEY] || {};
     }
     
+    // // Save tag properties to notebook metadata
+    // private static saveTagProperties(notebook: vscode.NotebookDocument, properties: Record<string, TagProperties>): Thenable<boolean> {
+    //     // Call updateNotebookMetadata with the proper path and value
+    //     return updateNotebookMetadata(notebook, [this.METADATA_KEY], properties)
+    //         .then(() => true)  // Convert void return to Thenable<boolean>
+    //         .catch(error => {
+    //             console.error('Error saving tag properties:', error);
+    //             return false;
+    //         });
+
+    //     // const edit = new vscode.WorkspaceEdit();
+    //     // const metadata = { ...notebook.metadata } || {};
+    //     // metadata[this.METADATA_KEY] = properties;
+        
+    //     // edit.replaceNotebookMetadata(notebook.uri, metadata);
+    //     // return vscode.workspace.applyEdit(edit);
+    // }
+
     // Save tag properties to notebook metadata
     private static saveTagProperties(notebook: vscode.NotebookDocument, properties: Record<string, TagProperties>): Thenable<boolean> {
-        const edit = new vscode.WorkspaceEdit();
-        const metadata = { ...notebook.metadata } || {};
-        metadata[this.METADATA_KEY] = properties;
-        
-        edit.replaceNotebookMetadata(notebook.uri, metadata);
-        return vscode.workspace.applyEdit(edit);
+        // Call updateNotebookMetadata with the proper path and value
+        return Promise.resolve(updateNotebookMetadata(notebook, [this.METADATA_KEY], properties))
+            .then(() => true)  // Convert void return to Thenable<boolean>
+            .catch(error => {
+                console.error('Error saving tag properties:', error);
+                return false;
+            });
     }
+
 }
