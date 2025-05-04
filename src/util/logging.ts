277f59366be012cab88eef545cf6a1ddfc90223d
Pho Hale <PhoHale@gmail.com>
2025-05-04 07:48:10 -0400

    improved logging robustness

--- src/util/logging.ts
+++ src/util/logging.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 import * as vscode from 'vscode';
-// import { log, showTimedInformationMessage } from '../notebookRunGroups/util/logging';
+// import { log, showTimedInformationMessage } from '../util/logging';
 // const myChannel = vscode.window.createOutputChannel('MyLogger');
 
 let myChannel: vscode.OutputChannel;
@@ -14,7 +14,7 @@ export function log(...args: any[]) {
     // showTimedInformationMessage(args.map(arg => (typeof arg === 'object' ? JSON.stringify(arg) : String(arg))).join(' '), 1000);
 }
 
-export async function showTimedInformationMessage(message: string, timeout: number) {
+export async function showTimedInformationMessage(message: string, timeout: number = 1000) {
     // Create the information message
     const messagePromise = vscode.window.showInformationMessage(message);
     // Create a timeout promise that resolves after a certain delay
@@ -28,21 +28,20 @@ export async function showTimedInformationMessage(message: string, timeout: numb
     await Promise.race([messagePromise, timeoutPromise]);
 }
 
-// export function showTimedErrorMessage(message: string, timeout: number) {
-//     // Create the information message
-//     const messagePromise = vscode.window.showErrorMessage(message);
-//     // Create a timeout promise that resolves after a certain delay
-//     const timeoutPromise = new Promise<void>(resolve => {
-//         setTimeout(() => {
-//             resolve();
-//         }, timeout);
-//     });
 
-//     // Use Promise.race to resolve whichever promise comes first (message dismissal or timeout)
-//     return Promise.race([messagePromise, timeoutPromise]).then(() => {
-//         // The message will disappear after the timeout
-//     });
-// }
+export async function showTimedErrorMessage(message: string, timeout: number = 5000) {
+    // Create the error message
+    const messagePromise = vscode.window.showErrorMessage(message);
+    // Create a timeout promise that resolves after a certain delay
+    const timeoutPromise = new Promise<void>(resolve => {
+        setTimeout(() => {
+            resolve();
+        }, timeout);
+    });
+
+    // Use Promise.race to resolve whichever promise comes first (message dismissal or timeout)
+    await Promise.race([messagePromise, timeoutPromise]);
+}
 
 
 // // Usage example: Show the message for 3 seconds (3000 ms)
